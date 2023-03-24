3cf38e81e980189033d4c8348b7927303dc6f171
tomqiluo <71529685+tomqiluo@users.noreply.github.com>
2023-03-24 13:50:04 -0400

    Use timer to cycle GREEN, RED, YELLOW

--- mytraffic.c
+++ mytraffic.c
@@ -1,62 +1,51 @@
-/* Necessary includes for device drivers */
-#include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kernel.h> /* printk() */
-#include <linux/slab.h> /* kmalloc() */
-#include <linux/fs.h> /* everything... */
-#include <linux/errno.h> /* error codes */
-#include <linux/types.h> /* size_t */
-#include <linux/proc_fs.h>
-#include <linux/fcntl.h> /* O_ACCMODE */
-#include <asm/system_misc.h> /* cli(), *_flags */
-#include <linux/uaccess.h>
-#include <asm/uaccess.h> /* copy_from/to_user */
+#include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/timer.h>
 
-MODULE_LICENSE("Dual BSD/GPL");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Qi Luo");
+MODULE_DESCRIPTION("A simple kernel module");
 
-#DEFINE RED_GPIO 1
-#DEFINE YELLOW_GPIO 2
-#DEFINE GREEN_GPIO 3
+static int time_cycle = 1000; // default cycle time is 1 second
+module_param(time_cycle, int, 0644); // allow time_cycle to be set as a module parameter
 
-#DEFINE CYCLE_RATE 1
+static struct timer_list my_timer;
 
-// STATES  0		1			2
-//	 normal      flashing-red 	flashing-yellow
+static int cycle_count = 0;
 
-int state;
-int red_light_flag,
-yellow light_flag,
-green_light_flag;
-
-void main(){
-state = 0;
-
-
-if(state = 0){
-
-
-
-
-}
-else if (state =1) {
-
-
-
-
-
-}
-else if (state =2) {
-
-
-
-
-
-
-
+void my_timer_callback(struct timer_list *timer)
+{
+    if (cycle_count == 0) {
+        printk(KERN_INFO "Green: %u\n", jiffies_to_msecs(jiffies)/1000);
+        mod_timer(&my_timer, jiffies + msecs_to_jiffies(time_cycle * 3));
+        cycle_count = cycle_count + 1;
+    } else if (cycle_count == 1) {
+        printk(KERN_INFO "Yellow: %u\n", jiffies_to_msecs(jiffies)/1000);
+        mod_timer(&my_timer, jiffies + msecs_to_jiffies(time_cycle));
+        cycle_count = cycle_count + 1;
+    } else if (cycle_count == 2) {
+        printk(KERN_INFO "Red: %u\n", jiffies_to_msecs(jiffies)/1000);
+        mod_timer(&my_timer, jiffies + msecs_to_jiffies(time_cycle * 2));
+        cycle_count = 0;
+    }
 }
 
-
+static int __init my_module_init(void)
+{
+    printk(KERN_INFO "My module loaded\n");
+    
+    timer_setup(&my_timer, my_timer_callback, 0);
+    mod_timer(&my_timer, jiffies + msecs_to_jiffies(1));
+    
+    return 0;
 }
 
+static void __exit my_module_exit(void)
+{
+    del_timer(&my_timer);
+    printk(KERN_INFO "My module unloaded\n");
+}
 
+module_init(my_module_init);
+module_exit(my_module_exit);
